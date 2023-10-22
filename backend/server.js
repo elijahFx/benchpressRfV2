ce538695437e23c68fb291bd2337868b02604cd3
elijahFx <killrodxgames@gmail.com>
2023-10-22 22:34:15 +0300

    new stuff

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const app = express()
 const cors = require("cors")
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: [""],
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true
+}))
 
 
 
